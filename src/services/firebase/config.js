c57b5375a4d1d27431521536e5d6abfb97242b5b
Matias Mattea <matias.mattea@gmail.com>
2025-12-29 15:25:47 -0300

    datos correctos de firebase

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
 
-  apiKey: "AIzaSyDKl2Ej29wL-6ACh9yjEU0kc4Q9FIXrRDg",
+const firebaseConfig = {
+  apiKey: "AIzaSyDKL2Ej29wL-6ACh9yjEU8kc409FIXrRDg",
   authDomain: "tienda-bomberos.firebaseapp.com",
   projectId: "tienda-bomberos",
   storageBucket: "tienda-bomberos.firebasestorage.app",
@@ -11,7 +11,5 @@ const firebaseConfig = {
   appId: "1:10447412640:web:6a61201c718b35e38018c5"
 };
 
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
