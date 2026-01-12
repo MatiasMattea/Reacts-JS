5d4a19cb185d49f34242f6d397f6817a4631d980
Matias Mattea <matias.mattea@gmail.com>
2026-01-12 09:41:25 -0300

    correcciones para vercel q da error

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import ItemList from './Itemlist';
+import ItemList from './ItemList';
 import { obtenerProductos, obtenerProductosPorCategoria } from '../services/productosService';
 
 const ItemListContainer = ({ greeting }) => {
